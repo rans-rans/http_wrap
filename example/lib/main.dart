ae13fa60e1541bcad6c5bee46aad8318536f64a0
rans <ransfordowusuansah9@gmail.com>
2026-06-04 09:32:58 +0000

    example/main.dart UI updates

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,10 +3,10 @@ import 'dart:async';
 
 import 'package:http_wrap/http_wrap.dart';
 
-final httpWrapPlugin = HttpWrap();
+final httpWrapPlugin = HttpWrap()..config(baseUrl: "https://fakerapi.it");
+
 void main() {
   runApp(const MyApp());
-  httpWrapPlugin.config(baseUrl: "https://sendgrid-v3-api.mock.beeceptor.com");
 }
 
 class MyApp extends StatefulWidget {
@@ -33,12 +33,18 @@ class _MyAppState extends State<MyApp> {
     _batchIdCtrl.dispose();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
+  /// Platform messages are asynchronous, so we initialize in an async method.
   Future<HttpResponse> _getData() async {
+    if (_batchIdCtrl.text.isEmpty) {
+      _batchIdCtrl.text = "1";
+    }
     return await httpWrapPlugin.request(
       method: .get,
-      endpoint: "/v3/user/scheduled_sends/${_batchIdCtrl.text}",
-      headers: {"on-behalf-of": "Excepteur veniam"},
+      endpoint: "/api/v2/books",
+      queryParams: {
+        "_quantity": _batchIdCtrl.text,
+        "_locale": "en_US",
+      },
     );
   }
 
@@ -46,7 +52,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('Plugin example app')),
+        appBar: AppBar(title: const Text('HTTP WRAP')),
         body: FutureBuilder(
           future: _getServerData,
           builder: (context, snapshot) {
@@ -71,43 +77,66 @@ class _MyAppState extends State<MyApp> {
               );
             }
 
-            final requestData = (snapshot.data?.data as List<Map>?) ?? [];
+            final data = (snapshot.data?.data as Map?) ?? {};
+            final requestData = data['data'] as List;
             return Center(
               child: SingleChildScrollView(
-                child: Column(
-                  spacing: 12,
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.sizeOf(context).width * 0.1,
-                      child: TextField(
-                        controller: _batchIdCtrl,
-                        keyboardType: .number,
-                        decoration: const InputDecoration(
-                          hintText: "Enter a batch number",
-                          border: OutlineInputBorder(),
-                          enabledBorder: OutlineInputBorder(),
-                          focusedBorder: OutlineInputBorder(),
+                child: Padding(
+                  padding: const .all(8.0),
+                  child: Column(
+                    spacing: 12,
+                    children: [
+                      Text("Enter a number and make press the button"),
+                      SizedBox(
+                        width: MediaQuery.sizeOf(context).width * 0.2,
+                        child: TextField(
+                          controller: _batchIdCtrl,
+                          textAlign: .center,
+                          keyboardType: .number,
+                          decoration: const InputDecoration(
+                            hintText: "Eg; 3",
+                            border: OutlineInputBorder(),
+                            enabledBorder: OutlineInputBorder(),
+                            focusedBorder: OutlineInputBorder(),
+                          ),
                         ),
                       ),
-                    ),
-                    ElevatedButton(
-                      child: const Text("Make Request"),
-                      onPressed: () {
-                        _getServerData = _getData();
-                        setState(() {});
-                      },
-                    ),
+                      ElevatedButton(
+                        child: const Text("Make Request"),
+                        onPressed: () {
+                          _getServerData = _getData();
+                          setState(() {});
+                        },
+                      ),
 
-                    ...requestData.map(
-                      (e) => ListTile(
-                        title: Text(e['batch_id']),
-                        subtitle: Text(
-                          e['status'],
-                          style: const .new(fontWeight: .bold),
+                      ...requestData.map(
+                        (e) => Container(
+                          alignment: .topLeft,
+                          padding: .all(12),
+                          decoration: BoxDecoration(
+                            borderRadius: .all(.circular(16)),
+                            boxShadow: [
+                              .new(blurRadius: 0.7, blurStyle: .outer),
+                            ],
+                          ),
+                          child: Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                              Text(
+                                e['title'],
+                                style: .new(fontSize: 16, fontWeight: .bold),
+                              ),
+                              Text(
+                                e['author'],
+                                style: const .new(fontWeight: .bold),
+                              ),
+                              Text(e['description']),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             );
