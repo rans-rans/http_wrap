11a1e872d48527d83ae0a0feb18b663a78b52c1b
rans <ransfordowusuansah9@gmail.com>
2026-08-05 13:50:48 +0000

    fixed KGP  warning by updating dependencies

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -10,7 +10,6 @@ allprojects {
 
 plugins {
     id("com.android.library")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -7,6 +7,7 @@ pluginManagement {
 }
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("com.android.library") version "9.0.1" apply false
 }
 
 rootProject.name = "http_wrap"
